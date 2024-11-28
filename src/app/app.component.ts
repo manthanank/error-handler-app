bab2c2ce7a185bde1fd6a1b30e4fcb501602d74d
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-11-28 22:53:49 +0530

    Update Angular dependencies to version 19 and modify AppComponent to include standalone property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone: false
 })
 export class AppComponent {
   inputValue = signal<string>('');
